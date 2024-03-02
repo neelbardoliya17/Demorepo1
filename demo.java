d69c843e510c70f8151e89dad9d04f761169168a
Neel Bardoliya <neelbardoliya4@gmail.com>
2024-03-02 12:09:06 +0530

    new feature added

--- demo.java
+++ demo.java
@@ -1,8 +1,13 @@
 import java.util.*;
 class demo
 {
+    static int Test()
+    {
+        return 5;
+    }
     public static void main(String args[])
     {
         System.out.println("neel here");
+        System.out.println(demo.Test());
     }
 }
